a5796f1a9f68e1ed2b99030b050c0865b29f0614
antoniocesar <alopes.java@gmail.com>
2024-07-08 23:38:57 -0300

    initial commit

---
+++ fileuploaderservice/README.md
@@ -0,0 +1,2 @@
+# bank-file-processor
+PoC aiming to test a file processing flow containing banking information using Java, Spring, and various AWS resources.
---
+++ fileuploaderservice/src/test/java/com/alopes/fileuploaderservice/FileuploaderserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alopes.fileuploaderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileuploaderserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
